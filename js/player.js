b7659b2c1e3371f8f1a7bff63f23c61c3510b0ac
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-07-02 09:36:09 +0200

    add inappropriate usernames and rename files

--- js/player.js
+++ js/player.js
@@ -1,5 +1,45 @@
 const simplePop = require("../js/utility");
 
+let inappropriateUsernames = [
+    "admin", "root", "123456", "password", "12345", "2g1c", "2 girls 1 cup", "acrotomophilia",
+    "alabama hot pocket", "alaskan pipeline", "anal", "anilingus", "anus", "apeshit", "arsehole",
+    "ass", "asshole", "assmunch", "auto erotic", "autoerotic", "babeland", "baby batter", "baby juice",
+    "ball gag", "ball gravy", "ball kicking", "ball licking", "ball sack", "ball sucking", "bangbros", "bangbus",
+    "bareback", "barely legal", "barenaked", "bastard", "bastardo", "bastinado", "bbw", "bdsm", "beaner", "beaners",
+    "beaver cleaver", "beaver lips", "beastiality", "bestiality", "big black", "big breasts", "big knockers", "big tits",
+    "bimbos", "birdlock", "bitch", "bitches", "black cock", "blonde action", "blonde on blonde action", "blowjob",
+    "blow job", "blow your load", "blue waffle", "blumpkin", "bollocks", "bondage", "boner", "boob", "boobs", "booty call",
+    "brown showers", "brunette action", "bukkake", "bulldyke", "bullet vibe", "bullshit", "bung hole", "bunghole", "busty",
+    "butt", "buttcheeks", "butthole", "camel toe", "camgirl", "camslut", "camwhore", "carpet muncher", "carpetmuncher",
+    "chocolate rosebuds", "cialis", "circlejerk", "cleveland steamer", "clit", "clitoris", "clover clamps",
+    "clusterfuck", "cock", "cocks", "coprolagnia", "coprophilia", "cornhole", "coon", "coons", "creampie", "cum",
+    "cumming", "cumshot", "cumshots", "cunnilingus", "cunt", "darkie", "date rape", "daterape", "deep throat",
+    "deepthroat", "dendrophilia", "dick", "dildo", "dingleberry", "dingleberries", "dirty pillows", "dirty sanchez",
+    "doggie style", "doggiestyle", "doggy style", "doggystyle", "dog style", "dolcett", "domination", "dominatrix",
+    "dommes", "donkey punch", "double dong", "double penetration", "dp action", "dry hump", "dvda", "eat my ass",
+    "ecchi", "ejaculation", "erotic", "erotism", "escort", "eunuch", "fag", "faggot", "fecal", "felch", "fellatio",
+    "feltch", "female squirting", "femdom", "figging", "fingerbang", "fingering", "fisting", "foot fetish",
+    "footjob", "frotting", "fuck", "fuck buttons", "fuckin", "fucking", "fucktards", "fudge packer",
+    "fudgepacker", "futanari", "gangbang", "gang bang", "gay sex", "genitals", "giant cock", "girl on",
+    "girl on top", "girls gone wild", "goatcx", "goatse", "god damn", "gokkun", "golden shower", "goodpoop", "goo girl",
+    "goregasm", "grope", "group sex", "g-spot", "guro", "hand job", "handjob", "hard core", "hardcore", "hentai",
+    "homoerotic", "honkey", "hooker", "horny", "hot carl", "hot chick", "how to kill", "how to murder", "huge fat",
+    "humping", "incest", "intercourse", "jack off", "jail bait", "jailbait", "jelly donut", "jerk off", "jigaboo",
+    "jiggaboo", "jiggerboo", "jizz", "juggs", "kike", "kinbaku", "kinkster", "kinky", "knobbing", "leather restraint",
+    "leather straight jacket", "lemon party", "livesex", "lolita", "lovemaking", "make me come", "male squirting",
+    "masturbate", "masturbating", "masturbation", "menage a trois", "milf", "missionary position", "mong",
+    "motherfucker", "mound of ven",
+];
+
+function checkUserName(username) {
+    if (inappropriateUsernames.includes(username))
+        return false;
+    else {
+        return true;
+    }
+
+}
+
 const socket = io();
 
 const joinForm = document.getElementById("joinForm");
@@ -9,7 +49,15 @@ joinForm.addEventListener("submit", (event) => {
 
     const username = document.getElementById("username").value;
     const gameID = document.getElementById("gameID").value;
+    if (checkUserName(username)) {
+        socket.emit("joinGame", { username, gameID });
 
+        username.value = '';
+        gameID.value = '';
+    }
+    else {
+        alert("Nuh uhhh")
+    }
     socket.emit("joinGame", { username, gameID });
 
     var request = new XMLHttpRequest();
@@ -50,3 +98,4 @@ joinForm.addEventListener("submit", (event) => {
 socket.on("playerJoined", ({ username }) => {
     simplePop("success", `${username} has joined the game`)
 });
+
